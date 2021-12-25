a4c80e618c119270991567da7ff9c35a17b369c3
Markus Lindenberg <markus@lindenberg.io>
2021-12-25 01:58:11 +0100

    Fix node_exporter build

--- package/node_exporter/node_exporter.mk
+++ package/node_exporter/node_exporter.mk
@@ -9,6 +9,12 @@ NODE_EXPORTER_SITE = $(call github,prometheus,node_exporter,v$(NODE_EXPORTER_VER
 NODE_EXPORTER_LICENSE = Apache-2.0
 NODE_EXPORTER_LICENSE_FILES = LICENSE
 
+define NODE_EXPORTER_POST_PATCH_VENDOR
+	cd $(@D); $(HOST_GO_TARGET_ENV) GOPROXY=https://proxy.golang.org,direct $(GO_BIN) mod vendor -v
+endef
+
+NODE_EXPORTER_POST_PATCH_HOOKS += NODE_EXPORTER_POST_PATCH_VENDOR
+
 define NODE_EXPORTER_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/usr/lib/systemd/system $(NODE_EXPORTER_PKGDIR)/node_exporter.service
 endef
